1649b42b866eaf0a9cd5bac0ea14b616e90f4d37
Sahil Singh <kswayam2000@gmail.com>
2024-09-07 12:43:57 +0530

    initial commit

--- src/components/qr generator/QRGenerator.jsx
+++ src/components/qr generator/QRGenerator.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import {QRCode} from 'react-qr-code'
 function QRGenerator() {
+
+    const [input, setInput] = useState('')
+    const [qrGenerator, setQrGenerator] = useState('')
+
+    const handleGenerate = () => {
+        setQrGenerator(input)
+        setInput('')
+    }
+
   return (
-    <div>
-      
+    <div style={{
+        height : '100vh',
+        width : '100vw',
+        display : 'flex',
+        justifyContent : 'center',
+        alignItems : 'center',
+        flexDirection : 'column',
+        gap : '15px'
+    }}>
+      <input style={{ padding : '10px', borderRadius : '8px', border : 'none',}} type="text" placeholder='enter text here' value={input} onChange={(e) => setInput(e.target.value)} />
+      <button disabled={input && input.trim() !== '' ? false : true} onClick={handleGenerate}>Generate</button>
+      <div>
+        <QRCode id="qr-code" value={qrGenerator} size={200}/>
+      </div>
     </div>
   )
 }
